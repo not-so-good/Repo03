29f92c9f84656bc817e4e43388867179b5bbe1a7
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-03-30 21:32:20 +0200

    Sixth assignment

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    //Only change code below this line
+   var x = 9;
+   var y = 16;
+   var z = "My name is John!"; 
+   //Only change code above this line
+   x = x + 2;
+   y = y + 4;
+   z = z + " John!"; 
+
+   return [x, y, z];
+}
+
+console.log(main());
+
+module.exports = main;
