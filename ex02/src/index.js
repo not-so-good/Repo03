923e05c8515654c3673d5618ad6e64e8f9c9a685
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-03-30 20:48:07 +0200

    Second assignment

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this line
+    //This is comment with more than 5 letters
+    /*
+    This is comment with more than 5 letters
+    */
+    //Only change code above this line
+}
+
+main();
+
+module.exports = main;
